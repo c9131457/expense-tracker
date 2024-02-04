ec6e22bede2ffd58f02e7ffdd6ef42a98cf0dc0f
c0131457 <c9131457@gmail.com>
2024-02-04 07:50:04 -0600

    Added income expense component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
   import {ref, computed} from 'vue'
 
   const transactions = ref([
@@ -11,9 +12,27 @@
     {id: 5, text: 'Tax Return', amount: 3000},
   ])
 
-//get the total
-const total = computed(() => {
+  //get the total
+  const total = computed(() => {
     return transactions.value.reduce( (acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+  })
+
+  //get the income by adding all positive values
+  const income = computed( () => {
+    return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce( (acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+  })
+
+  //get the expenses by adding all negative values
+  const expense = computed( () => {
+    return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce( (acc, transaction) => {
       return acc + transaction.amount
     }, 0)
   })
@@ -23,5 +42,6 @@ const total = computed(() => {
   <Header></Header>
   <div class="container">
     <Balance :total="total"></Balance>
+    <IncomeExpenses :income="income" :expense="expense"></IncomeExpenses>
   </div>
 </template>
